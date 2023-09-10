2f85c9f4ee39990fba4aee3a6c84ed78a40a476b
maruf9270 <maruf9270@gmail.com>
2023-09-10 15:00:27 +0600

    Added book updating functionality

--- src/app/modules/book/book.service.ts
+++ src/app/modules/book/book.service.ts
@@ -2,8 +2,14 @@ import { Book } from "@prisma/client";
 import { prisma } from "../../shared/prisma";
 // For creating a new Book
 const postNewBook = async (params: Book) => {
+  const isoDate = params.publicationDate + "T00:00:00.000Z";
+  params.publicationDate = isoDate as unknown as Date;
+  console.log(isoDate);
   const result = await prisma.book.create({
     data: params,
+    include: {
+      category: true,
+    },
   });
 
   return result;
@@ -11,11 +17,18 @@ const postNewBook = async (params: Book) => {
 
 // FOr updaing
 const updateBook = async (params: Partial<Book>, id: string) => {
+  if (params.publicationDate) {
+    const isoDate = params.publicationDate + "T00:00:00.000Z";
+    params.publicationDate = isoDate as unknown as Date;
+  }
   const result = await prisma.book.update({
     where: {
       id: id,
     },
     data: params,
+    include: {
+      category: true,
+    },
   });
 
   return result;
